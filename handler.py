822afe07298750fe3a0c113852cd2d782d782f08
tornskin <piotr.daniec@gmail.com>
2023-08-02 12:16:04 +0200

    added all additional functionality and classes

--- handler.py
+++ handler.py
@@ -1,4 +1,6 @@
 import csv
+import json
+import pickle
 from typing import List
 
 
@@ -11,10 +13,7 @@ class FileHandler:
         self.matrix = []
 
     def read_data_from_file(self):
-        with open(self.input_file) as file:
-            reader = csv.reader(file)
-            for line in reader:
-                self.matrix.append(line)
+        raise NotImplementedError("read_data_from_file method must be implemented in the subclass.")
 
     def change_data_in_our_matrix(self):
         for incoming_change in self.changes_in_file:
@@ -22,16 +21,82 @@ class FileHandler:
                 data_to_change = incoming_change.split(",")
                 x_value = int(data_to_change[0])
                 y_value = int(data_to_change[1])
+
+                # Extend rows to required length
+                if x_value >= len(self.matrix):
+                    for i in range(len(self.matrix), x_value + 1):
+                        self.matrix.append([])
+
+                # Extend row to required length
+                if y_value >= len(self.matrix[x_value]):
+                    for i in range(len(self.matrix[x_value]), y_value + 1):
+                        self.matrix[x_value].append("")
+
                 self.matrix[x_value][y_value] = data_to_change[2]
+
             except ValueError:
                 print("Podano niepoprawne dane wejściowe:", incoming_change)
+            except IndexError:
+                print("Podane współrzędne są poza zakresem macierzy:", incoming_change)
 
     def write_data_to_file(self):
-        with open(self.output_file, mode="w") as file:
-            writer = csv.writer(file)
-            writer.writerows(self.matrix)
+        raise NotImplementedError("write_data_to_file method must be implemented in the subclass.")
 
     def run_program(self):
         self.read_data_from_file()
         self.change_data_in_our_matrix()
         self.write_data_to_file()
+
+
+class CSVHandler(FileHandler):
+
+    def read_data_from_file(self):
+        with open(self.input_file) as file:
+            reader = csv.reader(file)
+            for line in reader:
+                self.matrix.append(line)
+
+    def write_data_to_file(self):
+        with open(self.output_file, mode="w", newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(self.matrix)
+
+
+class JSONHandler(FileHandler):
+
+    def read_data_from_file(self):
+        with open(self.input_file) as file:
+            data = json.load(file)
+            if "matrix" in data:
+                self.matrix = data["matrix"]
+            else:
+                print("Nieobsługiwany format danych w pliku JSON.")
+
+    def write_data_to_file(self):
+        with open(self.output_file, mode="w") as file:
+            json.dump({"matrix": self.matrix}, file)
+
+
+class TXTHandler(FileHandler):
+
+    def read_data_from_file(self):
+        with open(self.input_file) as file:
+            for line in file:
+                row = line.strip().split(',')
+                self.matrix.append(row)
+
+    def write_data_to_file(self):
+        with open(self.output_file, mode="w") as file:
+            for row in self.matrix:
+                file.write(','.join(row) + '\n')
+
+
+class PickleHandler(FileHandler):
+
+    def read_data_from_file(self):
+        with open(self.input_file, 'rb') as file:
+            self.matrix = pickle.load(file)
+
+    def write_data_to_file(self):
+        with open(self.output_file, 'wb') as file:
+            pickle.dump(self.matrix, file)
--- zadanie.py
+++ zadanie.py
@@ -1,18 +1,39 @@
 import sys
-
-from handler import FileHandler
+import os
+from handler import CSVHandler, JSONHandler, TXTHandler, PickleHandler
 
 if len(sys.argv) < 4:
     print("Podano za mało argumentów.")
     print("Użyj: python zadanie.py input_file output_file changes_in_file")
     sys.exit(1)
 
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+changes_in_file = sys.argv[3:]
+
+if not os.path.exists(input_file):
+    print("Podany plik wejściowy nie istnieje.")
+    sys.exit(1)
+
+file_extension = input_file.split('.')[-1].lower()
+
+if file_extension == "csv":
+    handler = CSVHandler(input_file, output_file, changes_in_file)
+elif file_extension == "json":
+    handler = JSONHandler(input_file, output_file, changes_in_file)
+elif file_extension == "txt":
+    handler = TXTHandler(input_file, output_file, changes_in_file)
+elif file_extension == "pickle":
+    handler = PickleHandler(input_file, output_file, changes_in_file)
+else:
+    print(f"Nieobsługiwany format pliku: {file_extension}")
+    sys.exit(1)
+
 try:
-    FileHandler(
-        input_file=sys.argv[1],
-        output_file=sys.argv[2],
-        changes_in_file=sys.argv[3:]
-    ).run_program()
+    handler.run_program()
 except FileNotFoundError:
     print("Podany plik wejściowy nie istnieje.")
+    sys.exit(1)
+except ValueError as e:
+    print(f"Podano niepoprawne dane wejściowe: {e}")
     sys.exit(1)
